66a9927592f5fb91bd4ff12ef79c6debbe4be6e6
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-05-06 22:19:17 +0900

    [스프링 트랜잭션 이해] - 트랜잭션 예외 롤백, 커밋 활용

---
+++ springtx/src/main/java/hello/springtx/order/NotEnoughMoneyException.java
@@ -0,0 +1,7 @@
+package hello.springtx.order;
+
+public class NotEnoughMoneyException extends Exception {
+    public NotEnoughMoneyException(final String message) {
+        super(message);
+    }
+}
